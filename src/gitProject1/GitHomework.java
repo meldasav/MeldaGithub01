5c91297d09f82353059739cb20bc64d4bc89f436
meldasav <gmeldag83@hotmail.com>
2022-04-10 23:19:38 -0500

    my github project1

--- src/gitProject1/GitHomework.java
+++ src/gitProject1/GitHomework.java
@@ -3,6 +3,7 @@ package gitProject1;
 public class GitHomework {
     public static void main(String[] args) {
         System.out.println("my github homework");
+        System.out.println("melda github commit");
 
     }
 }
