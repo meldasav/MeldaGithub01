4980ee81e95882a39d6e987791b219edcd9656f6
meldasav <gmeldag83@hotmail.com>
2022-04-11 00:40:29 -0500

    my github project1

--- src/gitProject1/GitHomework.java
+++ src/gitProject1/GitHomework.java
@@ -4,6 +4,9 @@ public class GitHomework {
     public static void main(String[] args) {
         System.out.println("my github homework");
         System.out.println("melda github commit");
+        System.out.println("github very important");
+        System.out.println("github is a version control");
+        System.out.println("VSR");
 
     }
 }
